ec270602bba826f5dffbbac214667ac97b066a4b
Angel Napoles Napoles <angel.napolesqva@gmail.com>
2025-03-23 12:03:48 -0400

    remove prisma migrations

--- prisma/migrations/20250302030336_userconfigroles/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `nombres_rol` on the `UsuarioRolConfig` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "UsuarioRolConfig" DROP COLUMN "nombres_rol",
-ADD COLUMN     "roles" TEXT NOT NULL DEFAULT 'Administrador,Responsable,Directivo,Logístico,Usuario';
